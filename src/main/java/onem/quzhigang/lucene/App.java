d1d524d078729e4827b5f8271e67b1e78f04e964
屈志刚 <598700560@qq.com>
2018-01-08 17:18:30 +0800

    first commit!

---
+++ src/main/java/onem/quzhigang/lucene/App.java
@@ -0,0 +1,13 @@
+package onem.quzhigang.lucene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
